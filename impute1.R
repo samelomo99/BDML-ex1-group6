b1a18547c5786f5f0053831621b843f2cedb0a4d
Juanfe752t <jf.trianag@uniandes.edu.co>
2025-01-22 19:24:10 -0500

    Update impute1.R

--- impute1.R
+++ impute1.R
@@ -1,1 +1,18 @@
-# Include here your code for your first chosen imputation method
+# IMputing the missing values with the median
+db <- db  %>%
+  mutate(ingtot = ifelse(is.na(ingtot) == TRUE, median(db$ingtot, na.rm = TRUE) , ingtot))
+## get missing values
+is.na(db$y_salary_m) %>% table()
+## replace values:
+db %>% select(directorio,y_salary_m) %>% tail()
+db = db %>% 
+     group_by(directorio) %>% 
+     mutate(mean_y_salary_m = mean(y_salary_m,na.rm=T))
+
+db %>% select(directorio,y_salary_m,mean_y_salary_m) %>% tail()
+db = db %>%
+     mutate(y_salary_m = ifelse(test = is.na(y_salary_m)==T,
+                               yes = mean_y_salary_m,
+                               no = y_salary_m))
+
+db %>% select(directorio,y_salary_m,mean_y_salary_m) %>% tail()
